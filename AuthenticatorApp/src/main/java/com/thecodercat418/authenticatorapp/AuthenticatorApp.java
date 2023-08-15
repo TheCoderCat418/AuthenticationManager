9eaf165d2828bf8b643c34b7d95f8865cb90510f
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-08-14 21:52:28 -0500

    Started Project (gitignore)

---
+++ AuthenticatorApp/src/main/java/com/thecodercat418/authenticatorapp/AuthenticatorApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thecodercat418.authenticatorapp;
+
+/**
+ *
+ * @author ellio
+ */
+public class AuthenticatorApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
